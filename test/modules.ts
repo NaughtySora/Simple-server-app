e8e378f1358c6120bc2b57ac2ce4da56e2e9df66
Trofymenko Vladyslav <v.trofymenko@omisoft.net>
2025-07-22 19:17:42 +0300

    login tests

--- test/modules.ts
+++ test/modules.ts
@@ -40,97 +40,97 @@ describe('modules', async () => {
         const resource = { body: test.data } as any;
         await it('controller', async () => {
           if (test.throws) {
-            // await assert.rejects(async () => controller(resource));
+            await assert.rejects(async () => controller(resource));
           } else {
-            // await controller(resource);
+            await controller(resource);
           }
         });
         if (test.throws) continue;
         await it('handler', async () => {
-          // const result = await handler(test.data as any) as
-          //   { meta: { code: number, }, response: Tokens };
-          // assert.strictEqual(result.meta.code, 201);
-          // assert.ok(typeof result.response.access === "string");
-          // assert.ok(typeof result.response.access === "string");
+          const result = await handler(test.data as any) as
+            { meta: { code: number, }, response: Tokens };
+          assert.strictEqual(result.meta.code, 201);
+          assert.ok(typeof result.response.access === "string");
+          assert.ok(typeof result.response.access === "string");
         });
       }
     });
 
-    // await it('get', async () => {
-    //   const modules = getUser(services);
-    //   const credentials = {
-    //     email: "email@gmail.com",
-    //     nickname: "nick",
-    //     password: "hash",
-    //   };
-    //   const user = await mockStorage.repository.user.create(credentials, () => { });
-    //   await it('controller', async () => {
-    //     const controller = modules[0];
-    //     const session = {
-    //       id: user.userId,
-    //       email: credentials.email,
-    //     };
-    //     const access = await app.session.access(session);
-    //     const tests = data.http.bearer(access);
-    //     for (const test of tests) {
-    //       const resource = { headers: test.task } as any;
-    //       if (test.throws) {
-    //         await assert.rejects(async () => controller(resource));
-    //       } else {
-    //         await controller(resource);
-    //       }
-    //     }
-    //   });
-    //   await it('handler', async () => {
-    //     const id = user.userId;
-    //     const handler = modules[1];
-    //     const result = await handler(id) as { response: any, };
-    //     assert.strictEqual(result.response.nickname, credentials.nickname);
-    //     assert.strictEqual(result.response.email, credentials.email);
-    //     assert.strictEqual(result.response.id, id);
-    //   });
-    // });
+    await it('get', async () => {
+      const modules = getUser(services);
+      const credentials = {
+        email: "email@gmail.com",
+        nickname: "nick",
+        password: "hash",
+      };
+      const user = await mockStorage.repository.user.create(credentials, () => { });
+      await it('controller', async () => {
+        const controller = modules[0];
+        const session = {
+          id: user.userId,
+          email: credentials.email,
+        };
+        const access = await app.session.access(session);
+        const tests = data.http.bearer(access);
+        for (const test of tests) {
+          const resource = { headers: test.task } as any;
+          if (test.throws) {
+            await assert.rejects(async () => controller(resource));
+          } else {
+            await controller(resource);
+          }
+        }
+      });
+      await it('handler', async () => {
+        const id = user.userId;
+        const handler = modules[1];
+        const result = await handler(id) as { response: any, };
+        assert.strictEqual(result.response.nickname, credentials.nickname);
+        assert.strictEqual(result.response.email, credentials.email);
+        assert.strictEqual(result.response.id, id);
+      });
+    });
 
-    // await it('login', async () => {
-    //   const login = loginUser(services);
-    //   const create = registerUser(services);
-    //   const createHandler = create[1] as any;
-    //   const validCredentials = {
-    //     email: "cool@gmail.com",
-    //     nickname: "coolGuy",
-    //     password: "aA123coolGuy!",
-    //   };
-    //   await createHandler(validCredentials);
-    //   const tests = data.user.login.slice(0);
-    //   tests.push({
-    //     test: {
-    //       email: validCredentials.email,
-    //       password: validCredentials.password,
-    //     },
-    //     throws: false,
-    //   });
-    //   const valid: any = [];
-    //   await it('controller', async () => {
-    //     const controller = login[0];
-    //     for (const test of tests) {
-    //       const resource = { body: test.test } as any
-    //       if (test.throws) {
-    //         await assert.rejects(async () => controller(resource));
-    //       }
-    //       else {
-    //         valid.push(await controller(resource));
-    //       }
-    //     }
-    //   });
-    //   await it('handler', async () => {
-    //     const handler = login[1];
-    //     for (const test of valid) {
-    //       const result = await handler(test.test as any) as { response: any, };
-    //       assert.strictEqual(result.response.email, test.test.email);
-    //       assert.ok(typeof result.response.nickname === "string");
-    //       assert.ok(typeof result.response.id === "string");
-    //     }
-    //   });
-    // });
+    await it('login', async () => {
+      const login = loginUser(services);
+      const create = registerUser(services);
+      const createHandler = create[1] as any;
+      const validCredentials = {
+        email: "cool@gmail.com",
+        nickname: "coolGuy",
+        password: "aA123coolGuy!",
+      };
+      await createHandler(validCredentials);
+      const tests = data.user.login.slice(0);
+      tests.push({
+        test: {
+          email: validCredentials.email,
+          password: validCredentials.password,
+        },
+        throws: false,
+      });
+      const valid: any = [];
+      await it('controller', async () => {
+        const controller = login[0];
+        for (const test of tests) {
+          const resource = { body: test.test } as any
+          if (test.throws) {
+            await assert.rejects(async () => controller(resource));
+          }
+          else {
+            valid.push(await controller(resource));
+          }
+        }
+      });
+      await it('handler', async () => {
+        const handler = login[1];
+        for (const test of valid) {
+          const result = await handler(test as any) as { response: any, };
+          assert.strictEqual(result.response.email, test.email);
+          assert.ok(typeof result.response.nickname === "string");
+          assert.ok(typeof result.response.id === "string");
+        }
+      });
+    });
   });
 });
