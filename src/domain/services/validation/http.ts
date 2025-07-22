0ce36d27af313e941b9bc1254ceccbc82d30acf1
Trofymenko Vladyslav <v.trofymenko@omisoft.net>
2025-07-22 21:40:05 +0300

    tests for reset password, controller only

--- src/domain/services/validation/http.ts
+++ src/domain/services/validation/http.ts
@@ -19,6 +19,7 @@ export default (context: DomainServicesDependencies) => {
       if (!token) unauthorized();
       try {
         const data = await session.decode(token);
+        console.log("data: ",data)
         const id = (data as any)?.data?.data?.id;
         if (typeof id !== 'string') throw new Error('id is not defined');
         return id;
